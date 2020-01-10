a0bf6b2594f7ea7e8eeeb14b1e8c4cff3c7dc777
Santosh <SantoshKumar.Patra@efi.com>
2020-01-10 21:02:50 +0530

    python masterclass

---
+++ python masterclass/sample/string.py
@@ -0,0 +1,21 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + name)
+
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
+
+age=24
+print(age)
+
+print(type(age))
+print(type(greeting))
+
+age = "2 years"
+print(age)
+print(type(age))
